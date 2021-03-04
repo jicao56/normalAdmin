0db1f6940f3663959cc6055dab46e50e2682e0d2
hanf <861829150@qq.com>
2021-03-04 17:48:38 +0800

    update

--- handlers/app.py
+++ handlers/app.py
@@ -20,6 +20,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
