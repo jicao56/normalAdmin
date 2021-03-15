66705f4cf446632cb66bdb71960a61fec786918a
hanf <861829150@qq.com>
2021-03-15 14:12:34 +0800

    静态文件处理

--- handlers/app.py
+++ handlers/app.py
@@ -1,13 +1,26 @@
 # -*- coding: utf-8 -*-
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from handlers.routers import base, config, login, logout, menu, user, group, role, permission, ugrp, config_developer
 
 
 app = FastAPI()
 
+# 静态文件解析
+app.mount('/statics', StaticFiles(directory='statics'), name='static')
 
+# 跨域处理
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
+# 业务处理
 app.include_router(base.router)
 app.include_router(config.router)
 app.include_router(config_developer.router)
@@ -20,10 +33,3 @@ app.include_router(role.router)
 app.include_router(permission.router)
 app.include_router(ugrp.router)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=['*'],
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
-    allow_headers=["*"],
-)
